78b264306dce259c288d873105ab00b384eb31c4
RiseSun <li1284537512@outlook.com>
2021-12-03 23:20:05 +0800

    Update MonitorSample.java

--- concurrency/threadpool/MonitorSample.java
+++ concurrency/threadpool/MonitorSample.java
@@ -26,3 +26,29 @@ public class MonitorSample {
         executor.shutdown();
     }
 }
+
+/*
+output:
+timestamp[1638544231088]T: pool-1-thread-1, active count: 5, pool size: 5, max pool size: 10, task count: 8
+timestamp[1638544231088]T: pool-1-thread-3, active count: 5, pool size: 5, max pool size: 10, task count: 8
+timestamp[1638544231088]T: pool-1-thread-4, active count: 6, pool size: 6, max pool size: 10, task count: 9
+timestamp[1638544231088]T: pool-1-thread-5, active count: 6, pool size: 6, max pool size: 10, task count: 9
+timestamp[1638544231089]T: pool-1-thread-6, active count: 7, pool size: 7, max pool size: 10, task count: 10
+timestamp[1638544231088]T: pool-1-thread-2, active count: 5, pool size: 5, max pool size: 10, task count: 8
+timestamp[1638544231089]T: pool-1-thread-7, active count: 7, pool size: 7, max pool size: 10, task count: 10
+
+timestamp[1638544236127]active count: 7, pool size: 7, max pool size: 10, task count: 10, completed task count: 0, largest pool size: 7
+timestamp[1638544236127]active count: 7, pool size: 7, max pool size: 10, task count: 10, completed task count: 0, largest pool size: 7
+timestamp[1638544236127]active count: 7, pool size: 7, max pool size: 10, task count: 10, completed task count: 0, largest pool size: 7
+timestamp[1638544236127]T: pool-1-thread-6, active count: 7, pool size: 7, max pool size: 10, task count: 10
+timestamp[1638544236127]active count: 7, pool size: 7, max pool size: 10, task count: 10, completed task count: 0, largest pool size: 7
+timestamp[1638544236127]active count: 7, pool size: 7, max pool size: 10, task count: 10, completed task count: 2, largest pool size: 7
+timestamp[1638544236127]T: pool-1-thread-3, active count: 7, pool size: 7, max pool size: 10, task count: 10
+timestamp[1638544236127]active count: 7, pool size: 7, max pool size: 10, task count: 10, completed task count: 0, largest pool size: 7
+timestamp[1638544236127]active count: 7, pool size: 7, max pool size: 10, task count: 10, completed task count: 0, largest pool size: 7
+timestamp[1638544236127]T: pool-1-thread-7, active count: 7, pool size: 7, max pool size: 10, task count: 10
+
+timestamp[1638544241132]active count: 3, pool size: 3, max pool size: 10, task count: 10, completed task count: 7, largest pool size: 7
+timestamp[1638544241132]active count: 3, pool size: 3, max pool size: 10, task count: 10, completed task count: 7, largest pool size: 7
+timestamp[1638544241132]active count: 3, pool size: 3, max pool size: 10, task count: 10, completed task count: 7, largest pool size: 7
+*/
